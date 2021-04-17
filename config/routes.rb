1e36fd4ef70eedf83e5fb193613295d11ca13e37
EC2 Default User <ec2-user@ip-172-31-29-212.ec2.internal>
2021-04-17 16:19:35 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resource :favorites, only: [:create, :destroy]
+    
+    resources :book_comments, only: [:create, :destroy]
+  end
+  
   resources :users, only: [:show, :edit, :update, :index]
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_102902) do
+ActiveRecord::Schema.define(version: 2021_04_17_160110) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "usr_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "book_commetnts", force: :cascade do |t|
+    t.text "comment"
+    t.integer "usr_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.text "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
